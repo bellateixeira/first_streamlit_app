74d70f31c42747cae3b263444f01e3e71c603918
Isabella Teixeira <99225694+bellateixeira@users.noreply.github.com>
2023-07-19 16:18:29 -0400

    Update streamlit_app.py
    troubleshooting streamlit issues

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,17 @@
 import streamlit
 
-streamlit.title('My Parents New Healthy Diner')
+streamlit run my_app.py --server.enableCORS=false
 
-streamlit.header('🥣 Breakfast Menu')
-streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
-streamlit.text('🥑🍞 Hard-Boiled Free-Range Egg')
+#streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+#streamlit.header('🥣 Breakfast Menu')
+#streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
+#streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
+#streamlit.text('🥑🍞 Hard-Boiled Free-Range Egg')
 
-import pandas
+#streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
+#import pandas
+
+#my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+#streamlit.dataframe(my_fruit_list)
