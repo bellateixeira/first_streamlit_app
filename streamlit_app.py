56b56d998ebb98f594e7359ec79ec450d417c2e4
Isabella Teixeira <99225694+bellateixeira@users.noreply.github.com>
2023-07-20 08:48:34 -0400

    Update streamlit_app.py
    added a multi-select option to smoothies and displayed data

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,20 @@
 import streamlit
 
-streamlit run my_app.py --server.enableCORS=false
+streamlit.title('My Parents New Healthy Diner')
 
-#streamlit.title('My Parents New Healthy Diner')
+streamlit.header('🥣 Breakfast Menu')
+streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥑🍞 Hard-Boiled Free-Range Egg')
 
-#streamlit.header('🥣 Breakfast Menu')
-#streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
-#streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
-#streamlit.text('🥑🍞 Hard-Boiled Free-Range Egg')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-#streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+import pandas
 
-#import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
-#my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-#streamlit.dataframe(my_fruit_list)
+# Let's put a pick list here so they can pick the fruit they want to include 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+# Display the table on the page.
+streamlit.dataframe(my_fruit_list)
